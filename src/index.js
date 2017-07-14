1245145e60859613de640062d30c277c6ae24837
EGOIST <0x142857@gmail.com>
2017-07-14 18:41:07 +0800

    fix lint

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ export default {
   render(h, ctx) {
     const { language } = ctx.props
     const code = ctx.props.code || ctx.children[0].text
-    const { value:html } = language ? hljs.highlight(language, code) : hljs.highlightAuto(code)
+    const { value: html } = language
+      ? hljs.highlight(language, code)
+      : hljs.highlightAuto(code)
 
     return h('pre', ctx.data, [
       h('code', {
